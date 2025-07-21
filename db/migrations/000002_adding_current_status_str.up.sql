571d31a085af7a4751c64c73117fbb679e686631
Jaden <jaden.kapali@gmail.com>
2025-07-21 10:09:24 -1000

    fixed migrations (removed populateTables & made default for current_status)

--- db/migrations/000002_adding_current_status_str.up.sql
+++ db/migrations/000002_adding_current_status_str.up.sql
@@ -2,4 +2,4 @@ ALTER TABLE bills
   RENAME COLUMN current_status TO current_status_string;
 
 ALTER TABLE bills
-  ADD COLUMN current_status VARCHAR(50);
+  ADD COLUMN current_status VARCHAR(50) DEFAULT '';
