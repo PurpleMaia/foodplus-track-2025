fbc52cb1b093e9cff59d47eeb6b0850feb22809a
Jaden <jaden.kapali@gmail.com>
2026-01-21 14:55:53 -1000

    edit url for cron job

--- server/scrape-bills.js
+++ server/scrape-bills.js
@@ -7,8 +7,6 @@ async function startScraping(url) {
     const individualBillsData = [];
     
     for (const bill of bills) {
-      console.log("ABOUT TO TEST THE SCRAPE INDIV");
-      console.log("bill.bill_url:", bill.bill_url);
       const billClassifier = bill.bill_url
       const individualBillData = await scrapeIndividual(billClassifier);
       if (individualBillData) {
@@ -30,31 +28,30 @@ async function startScraping(url) {
 }
 
 async function main() {
-  
-  const currentYear = new Date().getFullYear();
-  console.log(`Starting cron job scraping for year ${currentYear}...`);
-  const houseURL = `https://data.capitol.hawaii.gov/advreports/advreport.aspx?year=${currentYear}&report=deadline&active=true&rpt_type=&measuretype=hb&title=House%20Bills%20Introduced`;
-  const senateURL = `https://data.capitol.hawaii.gov/advreports/advreport.aspx?year=${currentYear}&report=deadline&active=true&rpt_type=&measuretype=sb&title=Senate%20Bills%20Introduced`;
+    const currentYear = new Date().getFullYear();
+    console.log(`Starting cron job, scraping for year ${currentYear}...`);
+
+    const houseURL = `https://data.capitol.hawaii.gov/advreports/advreport.aspx?year=${currentYear}&report=deadline&active=true&rpt_type=&measuretype=hb&title=House%20Bills%20with%20Action%20Taken%20in%20${currentYear}%20Only`;
+    const senateURL = `https://data.capitol.hawaii.gov/advreports/advreport.aspx?year=${currentYear}&report=deadline&active=true&rpt_type=&measuretype=sb&title=Senate%20Bills%20with%20Action%20Taken%20in%20${currentYear}%20Only`;
 
 
-  console.log('Scraping House bills...');
-  const startTime = Date.now();
-  
-  await startScraping(houseURL);
-  
-  const endTime = Date.now();
-  const duration = (endTime - startTime) / 1000 / 60; // in minutes
-  console.log(`Finished scraping House bills in ${duration} minutes.`);
+    console.log('[MAIN] Scraping House bills...');
+    const startTime = Date.now();
+    
+    await startScraping(houseURL);
+    
+    const endTime = Date.now();
+    const duration = (endTime - startTime) / 1000 / 60; // in minutes
+    console.log(`[MAIN] Finished scraping House bills in ${duration} minutes.`);
 
-  
-  console.log('Scraping Senate bills...');
-  const startTimeSenate = Date.now();
-  
-  await startScraping(senateURL);
+    console.log('[MAIN] Scraping Senate bills...');
+    const startTimeSenate = Date.now();
 
-  const endTimeSenate = Date.now();
-  const durationSenate = (endTimeSenate - startTimeSenate) / 1000 / 60; // in minutes
-  console.log(`Finished scraping Senate bills in ${durationSenate} minutes.`);
+    await startScraping(senateURL);
+
+    const endTimeSenate = Date.now();
+    const durationSenate = (endTimeSenate - startTimeSenate) / 1000 / 60; // in minutes
+    console.log(`[MAIN] Finished scraping Senate bills in ${durationSenate} minutes.`);
 }
 
 main().catch((error) => {
